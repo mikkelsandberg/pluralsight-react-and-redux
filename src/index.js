2c719a0e5520ee2a302a3f027f8144d6d1ce3a3e
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2019-07-16 14:06:24 -0700

    adding provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App';
+import configureStore from './redux/configureStore';
+
+const store = configureStore();
 
 render(
-	<Router>
-		<App />
-	</Router>,
+	<ReduxProvider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</ReduxProvider>,
 	document.getElementById('app')
 );
