b0234a1f1bcee13233efea32514dfb5a17e0df1f
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2019-07-16 13:42:57 -0700

    adding course form; adding first action

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
-
 class CoursesPage extends Component {
+	state = {
+		course: {
+			title: ''
+		}
+	};
+
+	handleChange = e => {
+		const course = { ...this.state.course, title: e.target.value };
+		this.setState({ course });
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+
+		alert(this.state.course.title);
+	};
+
 	render() {
-		return <h2>Courses</h2>;
+		const {
+			state: { course },
+
+			handleChange,
+			handleSubmit
+		} = this;
+
+		return (
+			<form onSubmit={handleSubmit}>
+				<h2>Courses</h2>
+				<h3>Add Course</h3>
+				<input type="text" onChange={handleChange} value={course.title} />
+				<button type="submit">Save</button>
+			</form>
+		);
 	}
 }
 
