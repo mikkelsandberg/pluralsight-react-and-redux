91e8c1c06477dd82074f33048a4830bc4a39cddf
Mikkel Sandberg <mikkel.h.sandberg@gmail.com>
2019-07-17 16:10:22 -0700

    changing ManageCoursePage to be function comp

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import AboutPage from './about/AboutPage';
 import Header from './common/Header';
 import CoursesPage from './courses/CoursesPage';
+import ManageCoursePage from './courses/ManageCoursePage';
 import HomePage from './home/HomePage';
 import PageNotFound from './PageNotFound';
 
@@ -14,6 +15,8 @@ const App = () => (
 			<Route exact path="/" component={HomePage} />
 			<Route path="/about" component={AboutPage} />
 			<Route path="/courses" component={CoursesPage} />
+			<Route path="/course/:slug" component={ManageCoursePage} />
+			<Route path="/course" component={ManageCoursePage} />
 			<Route component={PageNotFound} />
 		</Switch>
 	</div>
--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,34 +1,31 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { loadAuthors } from '../../redux/actions/authorActions';
 import { loadCourses } from '../../redux/actions/courseActions';
 
-class ManageCoursePage extends Component {
-	componentDidMount() {
-		const { courses, authors, loadAuthors, loadCourses } = this.props;
-		if (!courses.length) {
+const ManageCoursePage = ({ courses, authors, loadAuthors, loadCourses }) => {
+	useEffect(() => {
+		if (!courses?.length) {
 			loadCourses().catch(error => {
 				console.log(`Loading courses failed: ${error}`);
 			});
 		}
 
-		if (!authors.length) {
+		if (!authors?.length) {
 			loadAuthors().catch(error => {
 				console.log(`Loading courses failed: ${error}`);
 			});
 		}
-	}
+	}, []);
 
-	render() {
-		return (
-			<>
-				<h2>Manage Course</h2>
-			</>
-		);
-	}
-}
+	return (
+		<>
+			<h2>Manage Course</h2>
+		</>
+	);
+};
 
 ManageCoursePage.propTypes = {
 	courses: PropTypes.array.isRequired,
